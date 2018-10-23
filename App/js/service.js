506044a8e7d7cf01a0d49d2ded1631723e3cfbab
Lior Youngstein <ly0505262699@gmail.com>
2018-10-23 13:29:37 +0300

    more imgs

--- App/js/service.js
+++ App/js/service.js
@@ -1,7 +1,26 @@
 'use strict'
 
 
-var gImgs = [{ id: 1, url: 'img/angry man.jpg', keywords: ['happy'] }];
+var gImgs = [
+    { id: 1, url: 'img/angry man.jpg', keywords: ['happy'],
+    { id: 2, url: 'img/angry man.jpg', keywords: ['happy'],
+    { id: 3, url: 'img/angry man.jpg', keywords: ['happy'],
+    { id: 4, url: 'img/angry man.jpg', keywords: ['happy'],
+    { id: 5, url: 'img/angry man.jpg', keywords: ['happy'],
+    { id: 6, url: 'img/angry man.jpg', keywords: ['happy'],
+    { id: 7, url: 'img/angry man.jpg', keywords: ['happy'],
+    { id: 8, url: 'img/angry man.jpg', keywords: ['happy'],
+    { id: 9, url: 'img/angry man.jpg', keywords: ['happy'],
+    { id: 10, url: 'img/angry man.jpg', keywords: ['happy'],
+    { id: 11, url: 'img/angry man.jpg', keywords: ['happy'],
+    { id: 1, url: 'img/angry man.jpg', keywords: ['happy'],
+    { id: 1, url: 'img/angry man.jpg', keywords: ['happy'],
+    { id: 1, url: 'img/angry man.jpg', keywords: ['happy'],
+    { id: 1, url: 'img/angry man.jpg', keywords: ['happy'],
+    { id: 1, url: 'img/angry man.jpg', keywords: ['happy'],
+    { id: 1, url: 'img/angry man.jpg', keywords: ['happy'],
+
+}];
 
 var gMeme = {
     selectedImgId: 5,
