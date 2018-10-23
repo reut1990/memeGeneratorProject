d79254d9376c8e86fcc2b5c8d7452f89ce862331
Lior Youngstein <ly0505262699@gmail.com>
2018-10-23 14:24:28 +0300

    render imgs done

--- App/js/main.js
+++ App/js/main.js
@@ -1,6 +1,28 @@
-onBiggerText(){
+// onBiggerText(){
    
-}
-onSmallerText(){
+// }
+// onSmallerText(){
 
+// }
+
+function init() {
+    renderProjs();
 }
+
+function renderProjs() {
+    var elPortfolioContainer = document.querySelector('.img-container');
+    var imgs = getImgs();
+    console.log(imgs);
+    var strHtmls = '';
+    for (var i = 0; i < imgs.length; i++) {
+        var img = imgs[i];
+        strHtmls += `
+        <li>
+        <img src=${img.url} alt="">
+    </li>
+        `;
+    }
+    elPortfolioContainer.innerHTML = strHtmls;
+}
+
+
